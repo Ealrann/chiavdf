b21a1770b32b8a370d1538733bdbd675252512fd
Aurélien Mora <ealrann@gmail.com>
2026-02-05 23:31:31 +0100

    optimizations

--- src/nucomp.h
+++ src/nucomp.h
@@ -50,177 +50,200 @@ typedef struct qfb
 
 typedef qfb qfb_t[1];
 
-// From Antic using Flint (works!)
-void qfb_nucomp(qfb_t r, const qfb_t f, const qfb_t g, mpz_t const& D, mpz_t const& L)
-{
-   mpz_t a1, a2, c2, ca, cb, cc, k, s, sp, ss, m, t, u2, v1, v2;
+namespace {
+struct NucompScratch {
+    mpz_t a1, a2, c2, ca, cb, cc, k, s, sp, ss, m, t, u2, v1, v2;
+    mpz_t m1, m2, r1, r2, co1, co2, temp;
 
-   if (mpz_cmp(f->a, g->a) > 0)
-   {
-      qfb_nucomp(r, g, f, D, L);
-      return;
-   }
+    NucompScratch() {
+        mpz_inits(a1, a2, c2, ca, cb, cc, k, s, sp, ss, m, t, u2, v1, v2, NULL);
+        mpz_inits(m1, m2, r1, r2, co1, co2, temp, NULL);
+    }
 
-   mpz_init(a1); mpz_init(a2); mpz_init(c2);
-   mpz_init(ca); mpz_init(cb); mpz_init(cc);
-   mpz_init(k); mpz_init(m);
-   mpz_init(s); mpz_init(sp); mpz_init(ss);
-   mpz_init(t); mpz_init(u2); mpz_init(v1); mpz_init(v2);
+    ~NucompScratch() {
+        mpz_clears(a1, a2, c2, ca, cb, cc, k, s, sp, ss, m, t, u2, v1, v2, NULL);
+        mpz_clears(m1, m2, r1, r2, co1, co2, temp, NULL);
+    }
 
-   /* nucomp calculation */
+    NucompScratch(const NucompScratch&) = delete;
+    NucompScratch& operator=(const NucompScratch&) = delete;
+};
 
-   mpz_set(a1, f->a);
-   mpz_set(a2, g->a);
-   mpz_set(c2, g->c);
-
-   mpz_add(ss, f->b, g->b);
-   mpz_fdiv_q_2exp(ss, ss, 1);
-
-   mpz_sub(m, f->b, g->b);
-   mpz_fdiv_q_2exp(m, m, 1);
-
-   mpz_fdiv_r(t, a2, a1);
-   if (!mpz_cmp_ui(t, 0))
-   {
-      mpz_set_ui(v1, 0);
-      mpz_set(sp, a1);
-   } else
-      mpz_gcdext(sp, v1, NULL, t, a1);
-
-   mpz_mul(k, m, v1);
-   mpz_fdiv_r(k, k, a1);
-
-   if (mpz_cmp_ui(sp, 1))
-   {
-      mpz_gcdext(s, v2, u2, ss, sp);
-
-      /* k = k*u2 - v2*c2 */
-      mpz_mul(k, k, u2);
-      mpz_mul(t, v2, c2);
-      mpz_sub(k, k, t);
-
-      if (mpz_cmp_ui(s, 1))
-      {
-         mpz_divexact(a1, a1, s);
-         mpz_divexact(a2, a2, s);
-         mpz_mul(c2, c2, s);
-      }
-
-      mpz_fdiv_r(k, k, a1);
-   }
-
-   if (mpz_cmp(a1, L) < 0)
-   {
-      mpz_mul(t, a2, k);
-
-      mpz_mul(ca, a2, a1);
-
-      mpz_mul_2exp(cb, t, 1);
-      mpz_add(cb, cb, g->b);
-
-      mpz_add(cc, g->b, t);
-      mpz_mul(cc, cc, k);
-      mpz_add(cc, cc, c2);
-
-      mpz_divexact(cc, cc, a1);
-   } else
-   {
-      mpz_t m1, m2, r1, r2, co1, co2, temp;
-
-      mpz_init(m1); mpz_init(m2); mpz_init(r1); mpz_init(r2);
-      mpz_init(co1); mpz_init(co2); mpz_init(temp);
-
-      mpz_set(r2, a1);
-      mpz_set(r1, k);
-
-      mpz_xgcd_partial(co2, co1, r2, r1, L);
-
-      /* m1 = (m*co1 + a2*r1) / a1 */
-      mpz_mul(t, a2, r1);
-      mpz_mul(m1, m, co1);
-      mpz_add(m1, m1, t);
-      mpz_divexact(m1, m1, a1);
-
-      /* m2 = (ss*r1 - c2*co1) / a1 */
-      mpz_mul(m2, ss, r1);
-      mpz_mul(temp, c2, co1);
-      mpz_sub(m2, m2, temp);
-      mpz_divexact(m2, m2, a1);
-
-      /* ca = -sgn(co1) * (r1*m1 - co1*m2) */
-      mpz_mul(ca, r1, m1);
-      mpz_mul(temp, co1, m2);
-      if (mpz_sgn(co1) < 0)
-         mpz_sub(ca, ca, temp);
-      else
-         mpz_sub(ca, temp, ca);
-
-      /* cb = (2 * (t - ca*co2) / co1 - g->b) % (2*ca) */
-      mpz_mul(cb, ca, co2);
-      mpz_sub(cb, t, cb);
-      mpz_mul_2exp(cb, cb, 1);
-      mpz_divexact(cb, cb, co1);
-      mpz_sub(cb, cb, g->b);
-      mpz_mul_2exp(temp, ca, 1);
-      mpz_fdiv_r(cb, cb, temp);
-
-      /* cc = (cb*cb - D) / (4*ca) */
-      mpz_mul(cc, cb, cb);
-      mpz_sub(cc, cc, D);
-      mpz_divexact(cc, cc, ca);
-      mpz_fdiv_q_2exp(cc, cc, 2);
-
-      if (mpz_sgn(ca) < 0)
-      {
-         mpz_neg(ca, ca);
-         mpz_neg(cc, cc);
-      }
-
-      mpz_clear(m1); mpz_clear(m2); mpz_clear(r1); mpz_clear(r2);
-      mpz_clear(co1); mpz_clear(co2); mpz_clear(temp);
-   }
-
-   mpz_set(r->a, ca);
-   mpz_set(r->b, cb);
-   mpz_set(r->c, cc);
-
-   mpz_clear(ca); mpz_clear(cb); mpz_clear(cc);
-   mpz_clear(k); mpz_clear(m);
-   mpz_clear(s); mpz_clear(sp); mpz_clear(ss);
-   mpz_clear(t); mpz_clear(u2); mpz_clear(v1); mpz_clear(v2);
-   mpz_clear(a1); mpz_clear(a2); mpz_clear(c2);
-}
-
-// a = b * c
-void nucomp_form(form &a, form const& b, form const& c, integer const& D, integer const& L) {
-    qfb fr, fr2, fr3;
-
-    *fr.a = *a.a.impl;
-    *fr.b = *a.b.impl;
-    *fr.c = *a.c.impl;
-    *fr2.a = *b.a.impl;
-    *fr2.b = *b.b.impl;
-    *fr2.c = *b.c.impl;
-    *fr3.a = *c.a.impl;
-    *fr3.b = *c.b.impl;
-    *fr3.c = *c.c.impl;
-
-    qfb_nucomp(&fr, &fr2, &fr3, D.impl, L.impl);
-
-    *a.a.impl = *fr.a;
-    *a.b.impl = *fr.b;
-    *a.c.impl = *fr.c;
-}
-
-void qfb_nudupl(qfb_t r, qfb_t f, mpz_t D, mpz_t L)
-{
+struct NuduplScratch {
     mpz_t a1, c1, cb, k, s, t, u2, v2;
+    mpz_t m2, r1, r2, co1, co2, temp;
 
-    mpz_init(a1); mpz_init(c1);
-    mpz_init(cb);
-    mpz_init(k);
-    mpz_init(s);
-    mpz_init(t); mpz_init(u2); mpz_init(v2);
+    NuduplScratch() {
+        mpz_inits(a1, c1, cb, k, s, t, u2, v2, NULL);
+        mpz_inits(m2, r1, r2, co1, co2, temp, NULL);
+    }
+
+    ~NuduplScratch() {
+        mpz_clears(a1, c1, cb, k, s, t, u2, v2, NULL);
+        mpz_clears(m2, r1, r2, co1, co2, temp, NULL);
+    }
+
+    NuduplScratch(const NuduplScratch&) = delete;
+    NuduplScratch& operator=(const NuduplScratch&) = delete;
+};
+
+// From Antic using Flint (works!)
+void qfb_nucomp_impl(
+    qfb_t r,
+    const qfb_t f,
+    const qfb_t g,
+    mpz_t const& D,
+    mpz_t const& L,
+    NucompScratch& scratch) {
+    mpz_t& a1 = scratch.a1;
+    mpz_t& a2 = scratch.a2;
+    mpz_t& c2 = scratch.c2;
+    mpz_t& ca = scratch.ca;
+    mpz_t& cb = scratch.cb;
+    mpz_t& cc = scratch.cc;
+    mpz_t& k = scratch.k;
+    mpz_t& s = scratch.s;
+    mpz_t& sp = scratch.sp;
+    mpz_t& ss = scratch.ss;
+    mpz_t& m = scratch.m;
+    mpz_t& t = scratch.t;
+    mpz_t& u2 = scratch.u2;
+    mpz_t& v1 = scratch.v1;
+    mpz_t& v2 = scratch.v2;
+    mpz_t& m1 = scratch.m1;
+    mpz_t& m2 = scratch.m2;
+    mpz_t& r1 = scratch.r1;
+    mpz_t& r2 = scratch.r2;
+    mpz_t& co1 = scratch.co1;
+    mpz_t& co2 = scratch.co2;
+    mpz_t& temp = scratch.temp;
+
+    if (mpz_cmp(f->a, g->a) > 0) {
+        qfb_nucomp_impl(r, g, f, D, L, scratch);
+        return;
+    }
+
+    /* nucomp calculation */
+
+    mpz_set(a1, f->a);
+    mpz_set(a2, g->a);
+    mpz_set(c2, g->c);
+
+    mpz_add(ss, f->b, g->b);
+    mpz_fdiv_q_2exp(ss, ss, 1);
+
+    mpz_sub(m, f->b, g->b);
+    mpz_fdiv_q_2exp(m, m, 1);
+
+    mpz_fdiv_r(t, a2, a1);
+    if (!mpz_cmp_ui(t, 0)) {
+        mpz_set_ui(v1, 0);
+        mpz_set(sp, a1);
+    } else {
+        mpz_gcdext(sp, v1, NULL, t, a1);
+    }
+
+    mpz_mul(k, m, v1);
+    mpz_fdiv_r(k, k, a1);
+
+    if (mpz_cmp_ui(sp, 1)) {
+        mpz_gcdext(s, v2, u2, ss, sp);
+
+        /* k = k*u2 - v2*c2 */
+        mpz_mul(k, k, u2);
+        mpz_mul(t, v2, c2);
+        mpz_sub(k, k, t);
+
+        if (mpz_cmp_ui(s, 1)) {
+            mpz_divexact(a1, a1, s);
+            mpz_divexact(a2, a2, s);
+            mpz_mul(c2, c2, s);
+        }
+
+        mpz_fdiv_r(k, k, a1);
+    }
+
+    if (mpz_cmp(a1, L) < 0) {
+        mpz_mul(t, a2, k);
+
+        mpz_mul(ca, a2, a1);
+
+        mpz_mul_2exp(cb, t, 1);
+        mpz_add(cb, cb, g->b);
+
+        mpz_add(cc, g->b, t);
+        mpz_mul(cc, cc, k);
+        mpz_add(cc, cc, c2);
+
+        mpz_divexact(cc, cc, a1);
+    } else {
+        mpz_set(r2, a1);
+        mpz_set(r1, k);
+
+        mpz_xgcd_partial(co2, co1, r2, r1, L);
+
+        /* m1 = (m*co1 + a2*r1) / a1 */
+        mpz_mul(t, a2, r1);
+        mpz_mul(m1, m, co1);
+        mpz_add(m1, m1, t);
+        mpz_divexact(m1, m1, a1);
+
+        /* m2 = (ss*r1 - c2*co1) / a1 */
+        mpz_mul(m2, ss, r1);
+        mpz_mul(temp, c2, co1);
+        mpz_sub(m2, m2, temp);
+        mpz_divexact(m2, m2, a1);
+
+        /* ca = -sgn(co1) * (r1*m1 - co1*m2) */
+        mpz_mul(ca, r1, m1);
+        mpz_mul(temp, co1, m2);
+        if (mpz_sgn(co1) < 0) {
+            mpz_sub(ca, ca, temp);
+        } else {
+            mpz_sub(ca, temp, ca);
+        }
+
+        /* cb = (2 * (t - ca*co2) / co1 - g->b) % (2*ca) */
+        mpz_mul(cb, ca, co2);
+        mpz_sub(cb, t, cb);
+        mpz_mul_2exp(cb, cb, 1);
+        mpz_divexact(cb, cb, co1);
+        mpz_sub(cb, cb, g->b);
+        mpz_mul_2exp(temp, ca, 1);
+        mpz_fdiv_r(cb, cb, temp);
+
+        /* cc = (cb*cb - D) / (4*ca) */
+        mpz_mul(cc, cb, cb);
+        mpz_sub(cc, cc, D);
+        mpz_divexact(cc, cc, ca);
+        mpz_fdiv_q_2exp(cc, cc, 2);
+
+        if (mpz_sgn(ca) < 0) {
+            mpz_neg(ca, ca);
+            mpz_neg(cc, cc);
+        }
+    }
+
+    mpz_set(r->a, ca);
+    mpz_set(r->b, cb);
+    mpz_set(r->c, cc);
+}
+
+void qfb_nudupl_impl(qfb_t r, qfb_t f, mpz_t D, mpz_t L, NuduplScratch& scratch) {
+    mpz_t& a1 = scratch.a1;
+    mpz_t& c1 = scratch.c1;
+    mpz_t& cb = scratch.cb;
+    mpz_t& k = scratch.k;
+    mpz_t& s = scratch.s;
+    mpz_t& t = scratch.t;
+    mpz_t& u2 = scratch.u2;
+    mpz_t& v2 = scratch.v2;
+    mpz_t& m2 = scratch.m2;
+    mpz_t& r1 = scratch.r1;
+    mpz_t& r2 = scratch.r2;
+    mpz_t& co1 = scratch.co1;
+    mpz_t& co2 = scratch.co2;
+    mpz_t& temp = scratch.temp;
 
     /* nucomp calculation */
 
@@ -265,11 +288,6 @@ void qfb_nudupl(qfb_t r, qfb_t f, mpz_t D, mpz_t L)
 
         mpz_fdiv_q(r->c, r->c, a1);
     } else {
-        mpz_t m2, r1, r2, co1, co2, temp;
-
-        mpz_init(m2); mpz_init(r1); mpz_init(r2);
-        mpz_init(co1); mpz_init(co2); mpz_init(temp);
-
         mpz_set(r2, a1);
         /* r1 = k */
         mpz_swap(r1, k);
@@ -287,8 +305,9 @@ void qfb_nudupl(qfb_t r, qfb_t f, mpz_t D, mpz_t L)
         mpz_mul(r->a, r1, r1);
         /* new_a = new_a - co1 * m2 */
         mpz_submul(r->a, co1, m2);
-        if (mpz_sgn(co1) >= 0)
+        if (mpz_sgn(co1) >= 0) {
             mpz_neg(r->a, r->a);
+        }
 
         mpz_mul(cb, r->a, co2);
         mpz_submul(cb, a1, r1);
@@ -310,18 +329,41 @@ void qfb_nudupl(qfb_t r, qfb_t f, mpz_t D, mpz_t L)
             mpz_neg(r->a, r->a);
             mpz_neg(r->c, r->c);
         }
-
-        mpz_clear(m2); mpz_clear(r1); mpz_clear(r2);
-        mpz_clear(co1); mpz_clear(co2); mpz_clear(temp);
     }
 
     mpz_set(r->b, cb);
+}
+} // namespace
 
-    mpz_clear(cb);
-    mpz_clear(k);
-    mpz_clear(s);
-    mpz_clear(t); mpz_clear(u2); mpz_clear(v2);
-    mpz_clear(a1); mpz_clear(c1);
+void qfb_nucomp(qfb_t r, const qfb_t f, const qfb_t g, mpz_t const& D, mpz_t const& L) {
+    thread_local NucompScratch scratch;
+    qfb_nucomp_impl(r, f, g, D, L, scratch);
+}
+
+// a = b * c
+void nucomp_form(form &a, form const& b, form const& c, integer const& D, integer const& L) {
+    qfb fr, fr2, fr3;
+
+    *fr.a = *a.a.impl;
+    *fr.b = *a.b.impl;
+    *fr.c = *a.c.impl;
+    *fr2.a = *b.a.impl;
+    *fr2.b = *b.b.impl;
+    *fr2.c = *b.c.impl;
+    *fr3.a = *c.a.impl;
+    *fr3.b = *c.b.impl;
+    *fr3.c = *c.c.impl;
+
+    qfb_nucomp(&fr, &fr2, &fr3, D.impl, L.impl);
+
+    *a.a.impl = *fr.a;
+    *a.b.impl = *fr.b;
+    *a.c.impl = *fr.c;
+}
+
+void qfb_nudupl(qfb_t r, qfb_t f, mpz_t D, mpz_t L) {
+    thread_local NuduplScratch scratch;
+    qfb_nudupl_impl(r, f, D, L, scratch);
 }
 
 // a = b * b
